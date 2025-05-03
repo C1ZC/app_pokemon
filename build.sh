b09d78aa03bb986a44d4b81910258e839a338e40
camilo zavala <{camilo.zavala.c@gmail.com}>
2025-05-03 00:26:21 -0400

    configuraciones

--- build.sh
+++ build.sh
@@ -2,6 +2,15 @@
 # exit on error
 set -o errexit
 
+# Instalar dependencias de SQL Server
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
+ACCEPT_EULA=Y apt-get install -y mssql-tools
+apt-get install -y unixodbc-dev
+
+
 # Actualizar pip
 python -m pip install --upgrade pip
 
